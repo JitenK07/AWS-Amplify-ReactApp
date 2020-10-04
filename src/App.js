f1fcf3729785b49a13466c907ac267ca5e34094f
Jitendra <jitendra.kumarj02@gmail.com>
2020-10-04 19:52:51 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         AWS Amplify Web application
+         AWS Amplify Web application deployeement
         </p>
         <a
           className="App-link"
